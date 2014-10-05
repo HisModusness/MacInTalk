0c4b3ccc67dd678cc3174f5beb2b4a74eb44ae6f
Liam Westby <lw322@nau.edu>
2014-10-05 16:37:50 -0500

    Initial commit of actual files

--- MacInTalk/AppDelegate.m
+++ MacInTalk/AppDelegate.m
@@ -11,16 +11,119 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (unsafe_unretained) IBOutlet NSTextView *textToSpeak;
+@property (weak) IBOutlet NSTableView *voiceTable;
+@property (weak) IBOutlet NSButton *speakButton;
+@property (weak) IBOutlet NSSlider *rateSlider;
+@property (weak) IBOutlet NSSlider *volumeSlider;
+
+@property (retain) NSSpeechSynthesizer *speechSynth;
+@property (retain) NSMutableArray *voices;
+@property (retain) NSMutableArray *voiceNames;
+
+@property BOOL speaking;
+@property NSInteger currentVoice;
+@property float rate;
+@property float volume;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+    self.speechSynth.delegate = self;
+    self.currentVoice = [self.voices indexOfObjectIdenticalTo:[self.speechSynth voice]];
+    
+    NSIndexSet *indexSet = [NSIndexSet indexSetWithIndex:self.currentVoice];
+    [self.voiceTable selectRowIndexes:indexSet byExtendingSelection:NO];
+    
+    [self.voiceTable scrollRowToVisible:self.currentVoice];
+    
+    self.rate = self.speechSynth.rate;
+    self.volume = self.speechSynth.volume;
+    
+    self.rateSlider.doubleValue = self.rate;
+    self.volumeSlider.doubleValue = self.volume;
+
+}
+
+- (void)awakeFromNib {
+    self.speaking = NO;
+    
+    self.voices = [[NSMutableArray alloc] initWithArray:[NSSpeechSynthesizer availableVoices]];
+    self.voiceNames = [[NSMutableArray alloc] init];
+
+    for (NSString *voice in _voices) {
+        NSArray *components = [voice componentsSeparatedByString:@"."];
+        if ([[components objectAtIndex:[components count] -1] isEqualToString:@"premium"]) {
+            [self.voiceNames addObject:[[components objectAtIndex:[components count]-2] capitalizedString]];
+        }
+        else {
+            [self.voiceNames addObject:[[components objectAtIndex:[components count]-1] capitalizedString]];
+        }
+    }
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+#pragma mark - IBActions
+
+- (IBAction)toggleSpeak:(id)sender {
+    if (_speaking) {
+        // Will stop speaking
+        [self.speechSynth stopSpeaking];
+        self.speakButton.title = @"Start";
+        self.speaking = NO;
+    }
+    else {
+        // Will start speaking
+        if (self.textToSpeak.string.length == 0) return;
+        [self.speechSynth startSpeakingString:self.textToSpeak.string];
+        self.speakButton.title = @"Stop";
+        self.speaking = YES;
+    }
+}
+- (IBAction)rateChanged:(id)sender {
+    self.rate = self.rateSlider.doubleValue;
+    self.speechSynth.rate = self.rate;
+}
+
+- (IBAction)volumeChanged:(id)sender {
+    self.volume = self.volumeSlider.doubleValue;
+    self.speechSynth.volume = self.volume;
+}
+
+#pragma mark - TableView Data Source
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    return [[self voiceNames] count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    if (tableColumn == [[[self voiceTable] tableColumns] objectAtIndex:0]) {
+        return [[self voiceNames] objectAtIndex:row];
+    }
+    return nil;
+}
+
+#pragma mark - Table View Delegate
+
+- (void)tableViewSelectionDidChange:(NSNotification *)aNotification {
+    self.currentVoice = self.voiceTable.selectedRow;
+    self.speechSynth.voice = [self.voices objectAtIndex:self.currentVoice];
+}
+
+#pragma mark - Speech Synthesizer Delegate 
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender didFinishSpeaking:(BOOL)finishedSpeaking {
+    self.speaking = NO;
+    self.speakButton.title = @"Speak";
+}
+
 
 @end
